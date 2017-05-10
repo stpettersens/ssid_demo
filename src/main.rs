f4431c23386c55ca632cde52ca5ef3e82b176f37
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-05-10 11:11:58 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,7 @@ fn main() {
   let ssid = SSID::new_query();
   let os = os_type::current_platform();
   println!("{:?}", ssid);
+  println!("SSID id = {} (state: {})", 
+  ssid.get_id(), ssid.get_state());
   println!("OS = {:?}", os);
 }
