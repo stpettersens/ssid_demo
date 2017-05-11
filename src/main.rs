ea6d695ed9cca5af5b0825c76b812988049c51b3
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-05-11 23:11:02 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 extern crate ssid;
-extern crate os_type;
 use ssid::SSID;
 
 fn main() {
   let ssid = SSID::new();
-  let os = os_type::current_platform();
   println!("{:#?}", ssid);
   println!("SSID id = {} (state: {})", 
   ssid.get_id(), ssid.get_state());
-  println!("OS = {:?}", os);
 }
